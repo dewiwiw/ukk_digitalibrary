2e5fe69f92e406b297cf24acc971ddd26a2faf5b
deui <157445937+dewiwiw@users.noreply.github.com>
2024-03-20 08:54:06 +0700

    Add files via upload

---
+++ ukk/admin/laporan.php
@@ -0,0 +1,32 @@
+<?php
+include 'layout/header.php';
+?>
+    <div class="container" style="margin-left: 25rem;">
+        <div class="row" style="margin-top: 2rem;">
+            <div class="col-sm-6">
+                <div class="card text-white text-center bg-primary">
+                    <h3 class="m-4">Laporan Data Anggota</h3>
+                    <a href="data/laporan_da.php" class="btn btn-dark btn-sm">Lihat Laporan</a>
+                </div>
+            </div>
+</div>
+<div class="row" style="margin-top: 2rem;">
+            <div class="col-sm-6">
+                <div class="card text-white text-center bg-primary">
+                    <h3 class="m-4">Laporan Data Buku</h3>
+                    <a href="data/laporan_db.php" class="btn btn-dark btn-sm">Lihat Laporan</a>
+                </div>
+            </div>
+</div>
+<div class="row" style="margin-top: 2rem;">
+            <div class="col-sm-6">
+                <div class="card text-white text-center bg-primary">
+                    <h3 class="m-4">Laporan Data Peminjaman</h3>
+                    <a href="data/laporan_dp.php" class="btn btn-dark btn-sm">Lihat Laporan</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include 'layout/footer.php';
+?>
---
+++ ukk/peminjam/layout/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Peminjam</title>
+    <link rel="shortcut icon" href="../asset/img/Logo SMK MTS.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h3 class="my-4"> <img src="../asset/img/logo smk mts.png" style="width: 50px;">Digitalibrary</h3>
+      <div class="card">
+        <div class="card-body bg-black">
+          <a href="index_pmj.php" class="btn text-white">Dashboard</a>
+          <a href="buku.php" class="btn text-white">Buku</a>
+          <a href="../logout.php" class="btn btn-danger">Logout</a>
+        </div>
+      </div>
+    </div>
